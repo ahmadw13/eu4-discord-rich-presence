3acf9fd2af0893ba7a38e13958ca5c4c26019ad3
Ahmed A <ahmadameer167@gmail.com>
2023-12-16 02:09:49 +0300

    complete refactoring and organising

--- main.py
+++ main.py
@@ -1,67 +1,97 @@
 from pypresence import Presence
-import time, os, psutil
-for proc in psutil.process_iter():
-    if proc.name() == "eu4.exe":
-        print("EU4 detected with pid: " + str(proc.pid))
-months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-savefile_path = os.environ['USERPROFILE'] + "\\Documents\\Paradox Interactive\\Europa Universalis IV\\save games\\"
-startepoch = time.time()
-flag = True
-RPC = Presence('687351243462541358')
-RPC.connect()
-RPC.update(state="Loading...",
-        large_text="Europa Universalis IV", large_image="eu4logolarge",
-        start=startepoch)
-try:
-    os.listdir(savefile_path)
-except:
-    print('using alternate savefile path...')
-    savefile_path = os.environ['USERPROFILE'] + "\\OneDrive\\Documents\\Paradox Interactive\\Europa Universalis IV\\save games\\"
-while(True):
-    save_list = os.listdir(savefile_path)
-    try:
-        savefile_name = save_list[0]
-        date_max = os.stat(savefile_path + save_list[0])
-    except:
-        print('Error! No save files found')
-        savefile_name = 'no saves found'
-    for sav in save_list:
-        date = os.stat(savefile_path + sav)
-        if date.st_mtime > date_max.st_mtime:
-            savefile_name = sav
-            #if not flag:
-            #    print('Newer save found!')
-            flag=True
+import time
+import os
+import psutil
+import logging
+# Constants
+EU4_PROCESS_NAME = "eu4.exe"
+DISCORD_APP_ID = '687351243462541358'
+SAVEFILE_PATHS = [os.path.join(os.environ['USERPROFILE'], 'Documents', 'Paradox Interactive', 'Europa Universalis IV', 'save games'),
+                  os.path.join(os.environ['USERPROFILE'], 'OneDrive', 'Documents', 'Paradox Interactive', 'Europa Universalis IV', 'save games')]
+MONTHS = ['January', 'February', 'March', 'April', 'May', 'June',
+          'July', 'August', 'September', 'October', 'November', 'December']
 
-    print("Using data from:", savefile_name)
+# Setup logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-    if(flag):
-        print("Changes found, updating...")
-        i=1
+
+def find_eu4_process():
+    for proc in psutil.process_iter(['pid', 'name']):
+        if proc.info['name'] == EU4_PROCESS_NAME:
+            logger.info(f"EU4 detected with PID: {proc.info['pid']}")
+            return True
+    return False
+
+
+def get_savefile_path():
+    for path in SAVEFILE_PATHS:
         try:
-            f=open(savefile_path + savefile_name,"r")
+            os.listdir(path)
+            return path
+        except FileNotFoundError:
+            continue
+    logger.error("No valid save file paths found.")
+    return None
+
+
+def update_discord_presence(file_path, presence, start_epoch):
+    try:
+        save_list = os.listdir(file_path)
+        if not save_list:
+            logger.warning("No save files found.")
+            return
+
+        savefile_name = max(save_list, key=lambda x: os.stat(
+            os.path.join(file_path, x)).st_mtime)
+        logger.info("Using data from: %s", savefile_name)
+
+        with open(os.path.join(file_path, savefile_name), "r") as f:
             contents = f.read()
-            f.close()
             listconts = contents.split()
+
             country_rank_num = listconts[listconts.index("human=yes") + 3][16:]
-            if(int(country_rank_num) == 1):
-                country_rank = "Duchy"
-            elif(int(country_rank_num) == 2):
-                country_rank = "Kingdom"
-            else:
-                country_rank = "Empire"
+            country_rank = "Duchy" if int(country_rank_num) == 1 else "Kingdom" if int(
+                country_rank_num) == 2 else "Empire"
+
             x = listconts.index("EU4txt")
-            country_name = listconts[x + 4][24:len(listconts[x + 4])-1]
-            current_month = months[int(listconts[x + 1][10:][:-1][:-1])-1]
+            country_name = listconts[x + 4][24:len(listconts[x + 4]) - 1]
+            current_month = MONTHS[int(listconts[x + 1][10:][:-1][:-1]) - 1]
             current_year = listconts[x + 1][5:9]
 
-            RPC.update(state=f"{current_month}, {current_year}",
-            details=f"{country_rank} of {country_name}",
-            large_text="Europa Universalis IV", large_image="eu4logolarge",
-            start=startepoch)
-            flag = False
-            print("Updated presence succesfully")
-        except:
-            print('Error! Can only read uncompressed autosaves')
-        print("Checking for changes...")
-    time.sleep(20)
+            presence.update(
+                state=f"{current_month}, {current_year}",
+                details=f"{country_rank} of {country_name}",
+                large_text="Europa Universalis IV",
+                large_image="eu4logolarge",
+                start=start_epoch
+            )
+            logger.info("Updated presence successfully.")
+
+    except Exception as e:
+        logger.error(f"Error while updating presence: {e}")
+
+
+def main():
+    presence = Presence(DISCORD_APP_ID)
+    presence.connect()
+    start_epoch = time.time()
+
+    try:
+        while find_eu4_process():
+            savefile_path = get_savefile_path()
+
+            if savefile_path:
+                update_discord_presence(savefile_path, presence, start_epoch)
+
+            time.sleep(20)
+
+    except KeyboardInterrupt:
+        logger.info("Script terminated by user.")
+
+    finally:
+        presence.close()
+
+
+if __name__ == "__main__":
+    main()
